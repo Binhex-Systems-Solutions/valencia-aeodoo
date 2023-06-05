a91f17459a30563807ea7a0c60b77b3282a6984b
Nicolás Ramos <n.ramos@binhex.es>
2023-06-05 15:06:05 +0100

    Update

--- l10n_es_atc_mod415/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2014-2023 Nicolás Ramos (http://binhex.es)
-# Copyright 2023 Binhex System Solutions
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import account_move
-from . import mod415
